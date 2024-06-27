c684726fb524913b28630971f40d943f42d6afbe
Jeetendar <jeetendarn626@gmail.com>
2024-06-27 22:51:53 +0530

    Create Crop_yield.py
    Crop Yield Prediction using Machine Learning" project aims to develop a robust predictive model that forecasts crop yields based on historical data and environmental factors. This project leverages machine learning algorithms to analyse past crop yields alongside variables such as weather conditions, soil quality, and agricultural practices to predict future yields accurately.

---
+++ Crop_yield.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.style.use("ggplot")
+
+df = pd.read_csv("yield_df.csv")
+
+df.head()
+
+df.drop('Unnamed: 0', axis=1, inplace=True)
+
+df.head()
+
+df.info()
+
+df.isnull().sum()
+
+df.duplicated().sum()
+
+df.drop_duplicates(inplace=True)
+
+df.duplicated().sum()
+
+df.shape
+
+df.describe()
+
+df.corr()
+
+len(df['Area'].unique())
+
+len(df['Item'].unique())
+
+plt.figure(figsize=(15,20))
+sns.countplot(y = df['Area'])
+plt.show()
+
+plt.figure(figsize=(15,20))
+sns.countplot(y = df['Item'])
+plt.show()
+
+(df['Area'].value_counts() <400).sum()
+
+country = df['Area'].unique()
+yield_per_country = []
+for state in country:
+    yield_per_country.append(df[df['Area'] == state]['hg/ha_yield'].sum())
+
+df['hg/ha_yield'].sum()
+
+yield_per_country
+
+plt.figure(figsize=(15,20))
+sns.barplot(y = country, x = yield_per_country)
+plt.show()
+
+crops = df['Item'].unique()
+yield_per_crop = []
+for crop in crops:
+    yield_per_crop.append(df[df['Item'] == crop]['hg/ha_yield'].sum())
+
+plt.figure(figsize=(15,20))
+sns.barplot(y = crops, x = yield_per_crop)
+plt.show()
+
+df.head()
+
+df.columns
+
+col = ['Year','average_rain_fall_mm_per_year','pesticides_tonnes', 'avg_temp','Area', 'Item', 'hg/ha_yield']
+
+df = df[col]
+
+df.head()
+
+X = df.drop('hg/ha_yield', axis = 1)
+y = df['hg/ha_yield']
+X.shape
+
+y.shape
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0, shuffle=True)
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+
+ohe = OneHotEncoder(drop = 'first')
+scale = StandardScaler()
+
+preprocesser = ColumnTransformer(
+    transformers = [
+        ('StandardScale', scale, [0,1,2,3]),
+        ('OneHotEncode', ohe, [4,5])
+    ], 
+    remainder = 'passthrough'
+) 
+X_train_dummy = preprocesser.fit_transform(X_train)
+X_test_dummy  = preprocesser.fit_transform(X_test)
+preprocesser.get_feature_names_out(col[:-1])
+
+from sklearn.linear_model import LinearRegression,Lasso, Ridge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error, r2_score, accuracy_score
+models = {
+    'Linear Regression': LinearRegression(),
+    'Lasso' : Lasso(),
+    'Ridge' : Ridge(),
+    'Decision Tree': DecisionTreeRegressor(),
+    'KNN': KNeighborsRegressor(),
+}
+
+for name, md in models.items():
+    md.fit(X_train_dummy,y_train)
+    y_pred = md.predict(X_test_dummy)
+    print(f"{name}: mae : {mean_absolute_error(y_test, y_pred)} score : {r2_score(y_test, y_pred)}")
+
+dtr = DecisionTreeRegressor()
+dtr.fit(X_train_dummy,y_train)
+dtr.predict(X_test_dummy)
+
+df.columns
+
+df.head()
+
+def prediction(Year, average_rain_fall_mm_per_year,pesticides_tonnes, avg_temp, Area, Item):
+    features = np.array([[Year, average_rain_fall_mm_per_year,pesticides_tonnes, avg_temp, Area, Item]], dtype = object)
+    transform_features = preprocesser.transform(features)
+    predicted_yeild = dtr.predict(transform_features).reshape(-1,1)
+    return predicted_yeild[0][0]
+result = prediction(1990,1485.0,121.0,16.37,'Albania','Maize')
+
+result
+
+
+
